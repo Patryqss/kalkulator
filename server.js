bf008a8a825ee85b8b889273698162bf56942875
Patryk Grzyb <gpatryk14@gmail.com>
2020-11-22 12:47:09 +0100

    Fixes in deployment files

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-const app = express()
+const app = express();
 
-app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist/spa'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/spa/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
